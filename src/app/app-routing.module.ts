3af9c86e65c336bf606ad37ec03e15cbd145cdee
Esteban Silvera <sistemas109@red5g.co>
2024-04-09 10:48:01 -0500

    creacion de routing auth y las rutas hijos del dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
 
 const routes: Routes = [
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
+
+  //Colocar las rutas que hicimos para saber donde estan todas las rutas
+  //ej: path: '/dashboard' PagesRouting
+  //ej: path: '/auth' AuthRouting
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 
   { path: '**', component: NopagefoundComponent },
 ];
@@ -18,6 +20,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes),
     PagesRoutingModule,
+    AuthRoutingModule,
   ],
 
   exports: [RouterModule],
