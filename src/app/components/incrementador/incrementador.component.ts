09977d9f88d51e02e03a5fa880e9b7836f7154d4
Esteban Silvera <sistemas109@red5g.co>
2024-04-10 09:13:58 -0500

    Final de componente incrementar

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -3,31 +3,44 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
-  styleUrls: ['./incrementador.component.css']
+  styleUrls: ['./incrementador.component.css'],
 })
 export class IncrementadorComponent {
-
   @Input('valor') progreso: number = 50;
+  @Input() btnClass: string = 'btn btn-info';
 
-  @Output() ValorSalida: EventEmitter<number> = new EventEmitter;
+  @Output() ValorSalida: EventEmitter<number> = new EventEmitter();
 
   cambiarValor(valor: number) {
-
     if (this.progreso >= 100 && valor >= 0) {
-      this.ValorSalida.emit(100)
+      this.ValorSalida.emit(100);
       this.progreso = 100;
-      return
+      return;
     }
 
     if (this.progreso <= 0 && valor <= 0) {
-      this.ValorSalida.emit(0)
+      this.ValorSalida.emit(0);
 
       this.progreso = 0;
-      return
+      return;
     }
 
     this.progreso = this.progreso + valor;
-    this.ValorSalida.emit(this.progreso)
+    this.ValorSalida.emit(this.progreso);
+  }
 
+  onChange(valor: number) {
+
+    console.log(valor)
+
+    if (valor >= 100) {
+      this.progreso = 100
+    } else if (valor <= 0) {
+      this.progreso = 0
+    }else{
+      this.progreso = valor
+    }
+
+    this.ValorSalida.emit(this.progreso);
   }
 }
